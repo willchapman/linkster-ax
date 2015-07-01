3ca4aa59bb85e08ef553e420774803f000bd006b
Will Chapman <will@raxware.com>
2015-07-01 09:36:13 -0400

    Code formatting and minor cleanup

--- src/com/raxware/linkster/BLinksterService.java
+++ src/com/raxware/linkster/BLinksterService.java
@@ -1,20 +1,20 @@
 /*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
  */
 package com.raxware.linkster;
 
@@ -22,36 +22,29 @@ import javax.baja.sys.*;
 
 public class BLinksterService extends BAbstractService {
 
-	private static String LINKSTER_TOOL_AGENT = "linkster:LinksterToolView";
-	
-	public Type[] getServiceTypes() {
-		return new Type[] { getType() };
-	}
-	
-	public BIcon getIcon()
-	{
-		return BIcon.std("link.png");
-	}
+    public Type[] getServiceTypes() {
+        return new Type[]{getType()};
+    }
 
-	/*-
-	 class BLinksterService
-	 {	 	
-	 }
-	 -*/
-/*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
-/*@ $com.raxware.linkster.BLinksterService(1239832666)1.0$ @*/
-/* Generated Sun Jul 27 20:12:50 EDT 2008 by Slot-o-Matic 2000 (c) Tridium, Inc. 2000 */
+    public BIcon getIcon() {
+        return BIcon.std("link.png");
+    }
 
+    /*-
+     class BLinksterService
+     {	 	
+     }
+     -*/
+    /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+    /*@ $com.raxware.linkster.BLinksterService(1239832666)1.0$ @*/
+    /* Generated Sun Jul 27 20:12:50 EDT 2008 by Slot-o-Matic 2000 (c) Tridium, Inc. 2000 */
 ////////////////////////////////////////////////////////////////
 // Type
 ////////////////////////////////////////////////////////////////
-  
-  public Type getType() { return TYPE; }
-  public static final Type TYPE = Sys.loadType(BLinksterService.class);
+    public Type getType() {
+        return TYPE;
+    }
+    public static final Type TYPE = Sys.loadType(BLinksterService.class);
 
-/*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
-
-
-
-
+    /*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
 }
--- src/com/raxware/linkster/qglobber/Globber.java
+++ src/com/raxware/linkster/qglobber/Globber.java
@@ -1,20 +1,20 @@
 /*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
  */
 package com.raxware.linkster.qglobber;
 
@@ -22,34 +22,34 @@ import java.util.*;
 
 /**
  * This works as the logic to expand a string into multiple strings using
- * a specified formatting language.  
- * 
- * This is only a preliminary work - needs to be revamped.  Need to expand on documentation as well.  
- * 
+ * a specified formatting language.
+ *
+ * This is only a preliminary work - needs to be revamped. Need to expand on documentation as well.
+ *
  * @author Will Chapman (elitecodex)
  *
  */
 public class Globber {
-	
-	public static String PLACE_HOLDER = "{[]}";
-	
-	/**
-     * Performs the logic for the <code>split</code> and 
+
+    public static String PLACE_HOLDER = "{[]}";
+
+    /**
+     * Performs the logic for the <code>split</code> and
      * <code>splitPreserveAllTokens</code> methods that do not return a
-     * maximum array length.  
+     * maximum array length.
      *
-     * CHANGES:  Renamed
-     * 
+     * CHANGES: Renamed
+     *
      * Borrowed from the Apache Foundation
      *
-     * @param str  the String to parse, may be <code>null</code>
-     * @param separatorChar the separate character
+     * @param str               the String to parse, may be <code>null</code>
+     * @param separatorChar     the separate character
      * @param preserveAllTokens if <code>true</code>, adjacent separators are
-     * treated as empty token separators; if <code>false</code>, adjacent
-     * separators are treated as one separator.
+     *                          treated as empty token separators; if <code>false</code>, adjacent
+     *                          separators are treated as one separator.
      * @return an array of parsed Strings, <code>null</code> if null String input
      */
-	public static String[] split(String str, char separatorChar, boolean preserveAllTokens) {
+    public static String[] split(String str, char separatorChar, boolean preserveAllTokens) {
         // Performance tuned for 2.0 (JDK1.4)
 
         if (str == null) {
@@ -58,7 +58,7 @@ public class Globber {
         int len = str.length();
         if (len == 0) {
             //return ArrayUtils.EMPTY_STRING_ARRAY;
-        	return new String[0];
+            return new String[0];
         }
         List list = new ArrayList();
         int i = 0, start = 0;
@@ -83,9 +83,10 @@ public class Globber {
         }
         return (String[]) list.toArray(new String[list.size()]);
     }
-	
-	/**
-     * <p>Checks if a String is empty ("") or null.</p>
+
+    /**
+     * <p>
+     * Checks if a String is empty ("") or null.</p>
      *
      * <pre>
      * StringUtils.isEmpty(null)      = true
@@ -95,24 +96,27 @@ public class Globber {
      * StringUtils.isEmpty("  bob  ") = false
      * </pre>
      *
-     * <p>NOTE: This method changed in Lang version 2.0.
+     * <p>
+     * NOTE: This method changed in Lang version 2.0.
      * It no longer trims the String.
      * That functionality is available in isBlank().</p>
      *
      * Borrowed from the Apache Foundation
      *
-     * @param str  the String to check, may be null
+     * @param str the String to check, may be null
      * @return <code>true</code> if the String is empty or null
      */
     public static boolean isEmpty(String str) {
         return str == null || str.length() == 0;
     }
-    
-	/**
-     * <p>Replaces a String with another String inside a larger String,
+
+    /**
+     * <p>
+     * Replaces a String with another String inside a larger String,
      * for the first <code>max</code> values of the search String.</p>
      *
-     * <p>A <code>null</code> reference passed to this method is a no-op.</p>
+     * <p>
+     * A <code>null</code> reference passed to this method is a no-op.</p>
      *
      * <pre>
      * StringUtils.replace(null, *, *, *)         = null
@@ -128,15 +132,15 @@ public class Globber {
      * StringUtils.replace("abaa", "a", "z", 2)   = "zbza"
      * StringUtils.replace("abaa", "a", "z", -1)  = "zbzz"
      * </pre>
-     * 
+     *
      * Borrowed from the Apache Foundation
      *
-     * @param text  text to search and replace in, may be null
-     * @param searchString  the String to search for, may be null
+     * @param text         text to search and replace in, may be null
+     * @param searchString the String to search for, may be null
      * @param replacement  the String to replace it with, may be null
-     * @param max  maximum number of values to replace, or <code>-1</code> if no maximum
+     * @param max          maximum number of values to replace, or <code>-1</code> if no maximum
      * @return the text with any replacements processed,
-     *  <code>null</code> if null String input
+     *         <code>null</code> if null String input
      */
     public static String replace(String text, String searchString, String replacement, int max) {
         if (isEmpty(text) || isEmpty(searchString) || replacement == null || max == 0) {
@@ -163,141 +167,137 @@ public class Globber {
         buf.append(text.substring(start));
         return buf.toString();
     }
-	
+
     /**
      * Takes a sequence and returns an integer array with the starting and
      * ending endpoints in elements 0 and 1 respectively.
-     * 
+     *
      * Format: &lt;START&gt;..&lt;END&gt;
-     * 
+     *
      * @param seq A string representing the sequence
-     * @return integer array.  The zero element is the START; the one element is the END 
+     * @return integer array. The zero element is the START; the one element is the END
      */
-	private static int[] breakSequence(String seq)
-	{
-		int[] endpoints = new int[2];
-		
-		int dotdot_pos = seq.indexOf("..");
-		
-		int start = Integer.parseInt(seq.substring(0, dotdot_pos));
-		int end = Integer.parseInt(seq.substring(dotdot_pos+2, seq.length()));
-		
-		endpoints[0] = start;
-		endpoints[1] = end;
-		
-		return endpoints;
-	}
-	
-	/**
-	 * Same as breakSequence, just returns the ASCII code for the starting and
-	 * ending characters.
-	 * 
-	 * REQUIREMENTS:  Must be explicitly asked for (prefixed with 'A:').  If not,
-	 * falls back to breakSequence and assumes integers  
-	 * 
-	 * @param seq
-	 * @return 
-	 */
-	private static int[] breakAlphaSequence(String seq)
-		throws IllegalArgumentException
-	{
-		// minimum valid length for an alpha sequence is 6
-		// A:a..b
-		
-		if ( seq.length() < 6 )
-			throw new IllegalArgumentException("Sequence too short for alpha-sequence");
-		
-		if ( seq.indexOf("A:") != -1 ) // alpha was explicitly asked for - good to go
-		{
-			int[] endpoints = new int[2];
-			
-			int dotdot_pos = seq.indexOf("..");
-			
-			String start = seq.substring(seq.indexOf("A:")+2, dotdot_pos);
-			String end = seq.substring(dotdot_pos+2, seq.length());
-			
-			if ( start.length() != 1 || end.length() != 1 )
-				throw new IllegalArgumentException("The parsed arguments are invalid ("+start+","+end+")");
-			
-			//System.out.println("start="+start);
-			//System.out.println("end="+end);
-			
-			endpoints[0] = start.charAt(0);
-			endpoints[1] = end.charAt(0);
-			
-			return endpoints;
-		}
-		else
-			return breakSequence(seq);
-		
-	}
-	
-	/**
-	 * Takes a single template and a single sequence and compiles them to
-     * an ArrayList of strings
-	 * 
-	 * @param t
-	 * @param s
-	 * @return
-	 */
-	public static ArrayList explodeSingleNumericalSequence(String t, String s)
-	{
-		ArrayList list = new ArrayList();
-		
-		// find out where the first placeholder is
-		int position = t.indexOf(PLACE_HOLDER);
-		int endPosition = position + PLACE_HOLDER.length();
-		
-		// break the string around the placeholder
-		String first = t.substring(0, endPosition);
-		String second = t.substring(endPosition, t.length());		
-		
-		// are we looking for an alpha sequence?
-		boolean alphaFlag = s.charAt(0)=='A' && s.charAt(1)==':';		
-		int[] endpoints = alphaFlag ? breakAlphaSequence(s) : breakSequence(s);
-		for(int a=endpoints[0]; a<=endpoints[1]; a++)
-		{
-			String val = alphaFlag ? Character.toString((char) a) : String.valueOf(a);
-			//first = replace(first,"{[]}", String.valueOf(a),-1);
-			list.add(replace(first,PLACE_HOLDER, val,-1) + second);
-		}
-		return list;
-	}
-        
+    private static int[] breakSequence(String seq) {
+        int[] endpoints = new int[2];
+
+        int dotdot_pos = seq.indexOf("..");
+
+        int start = Integer.parseInt(seq.substring(0, dotdot_pos));
+        int end = Integer.parseInt(seq.substring(dotdot_pos + 2, seq.length()));
+
+        endpoints[0] = start;
+        endpoints[1] = end;
+
+        return endpoints;
+    }
+
     /**
-     * This is for taking a single template string and using explodeSingle... to 
+     * Same as breakSequence, just returns the ASCII code for the starting and
+     * ending characters.
+     *
+     * REQUIREMENTS: Must be explicitly asked for (prefixed with 'A:'). If not,
+     * falls back to breakSequence and assumes integers
+     *
+     * @param seq
+     * @return
+     */
+    private static int[] breakAlphaSequence(String seq)
+            throws IllegalArgumentException {
+		// minimum valid length for an alpha sequence is 6
+        // A:a..b
+
+        if (seq.length() < 6) {
+            throw new IllegalArgumentException("Sequence too short for alpha-sequence");
+        }
+
+        if (seq.indexOf("A:") != -1) // alpha was explicitly asked for - good to go
+        {
+            int[] endpoints = new int[2];
+
+            int dotdot_pos = seq.indexOf("..");
+
+            String start = seq.substring(seq.indexOf("A:") + 2, dotdot_pos);
+            String end = seq.substring(dotdot_pos + 2, seq.length());
+
+            if (start.length() != 1 || end.length() != 1) {
+                throw new IllegalArgumentException("The parsed arguments are invalid (" + start + "," + end + ")");
+            }
+
+			//System.out.println("start="+start);
+            //System.out.println("end="+end);
+            endpoints[0] = start.charAt(0);
+            endpoints[1] = end.charAt(0);
+
+            return endpoints;
+        } else {
+            return breakSequence(seq);
+        }
+
+    }
+
+    /**
+     * Takes a single template and a single sequence and compiles them to
+     * an ArrayList of strings
+     *
+     * @param t
+     * @param s
+     * @return
+     */
+    public static ArrayList explodeSingleNumericalSequence(String t, String s) {
+        ArrayList list = new ArrayList();
+
+        // find out where the first placeholder is
+        int position = t.indexOf(PLACE_HOLDER);
+        int endPosition = position + PLACE_HOLDER.length();
+
+        // break the string around the placeholder
+        String first = t.substring(0, endPosition);
+        String second = t.substring(endPosition, t.length());
+
+        // are we looking for an alpha sequence?
+        boolean alphaFlag = s.charAt(0) == 'A' && s.charAt(1) == ':';
+        int[] endpoints = alphaFlag ? breakAlphaSequence(s) : breakSequence(s);
+        for (int a = endpoints[0]; a <= endpoints[1]; a++) {
+            String val = alphaFlag ? Character.toString((char) a) : String.valueOf(a);
+            //first = replace(first,"{[]}", String.valueOf(a),-1);
+            list.add(replace(first, PLACE_HOLDER, val, -1) + second);
+        }
+        return list;
+    }
+
+    /**
+     * This is for taking a single template string and using explodeSingle... to
      * create an ArrayList for each item in the sequence array
-     * 
+     *
      * @param t
      * @param s
      * @return
      */
     public static ArrayList explodeSequence(String t, String[] s, int index)
-        throws RuntimeException
-    {
-        if ( s==null || s.length <= 0 )
+            throws RuntimeException {
+        if (s == null || s.length <= 0) {
             throw new RuntimeException("Invalid sequence array");
-        
-        if ( t == null )
+        }
+
+        if (t == null) {
             throw new NullPointerException("Invalid template string");
-    
-        ArrayList list = new ArrayList(s.length*2);
+        }
+
+        ArrayList list = new ArrayList(s.length * 2);
         list.addAll(explodeSingleNumericalSequence(t, s[index]));
-        String lastElement = (String)list.get(list.size()-1);
-        if ( lastElement.indexOf(PLACE_HOLDER) > 0 && s.length > index )
-        {
+        String lastElement = (String) list.get(list.size() - 1);
+        if (lastElement.indexOf(PLACE_HOLDER) > 0 && s.length > index) {
             ArrayList sublist = new ArrayList();
-            for(int i=0; i<list.size(); i++)
-            {
-                ArrayList temp = explodeSequence((String)list.get(i), s, index+1);
+            for (int i = 0; i < list.size(); i++) {
+                ArrayList temp = explodeSequence((String) list.get(i), s, index + 1);
                 sublist.addAll(temp);
             }
             return sublist;
         }
-        
+
         return list;
     }
-    
+
     /**
      * @deprecated
      * @param t
@@ -306,41 +306,35 @@ public class Globber {
      * @throws RuntimeException
      */
     public static ArrayList explodeNumericalSequence(String t, String[] s)
-        throws RuntimeException
-    {
+            throws RuntimeException {
         return explodeSequence(t, s, 0);
     }
-	
+
     /**
      * Test driver.
-     * 
+     *
      * @param args
      */
-	public static void main(String[] args)
-	{
-        String[] seq = {"A:A..C", "1..5", "1..5","A:A..B"};
-		ArrayList files = Globber.explodeSequence
-			(
-					"slot:/Building{[]}/Floor{[]}/VAV{[]}{[]}",
-                                        seq,
-                                        0
-			);
-                /*
-                 String[] files = Globber.explodeNumericalSequence
-			(
-					"slot:/Building{[]}/Floor{[]}",
-					new String[] {"10..15", "1..3"}
-			);
-		*/
-		
-		System.out.println("Results\n================");
-		for(int i=0; i<files.size(); i++)
-		{
-			System.out.println("files["+i+"] = " + files.get(i));
-		}
-		
-		
-		//int[] test1 = breakAlphaSequence("A:a..f");
-	}
-}
+    public static void main(String[] args) {
+        String[] seq = {"A:A..C", "1..5", "1..5", "A:A..B"};
+        ArrayList files = Globber.explodeSequence(
+                "slot:/Building{[]}/Floor{[]}/VAV{[]}{[]}",
+                seq,
+                0
+        );
+        /*
+         String[] files = Globber.explodeNumericalSequence
+         (
+         "slot:/Building{[]}/Floor{[]}",
+         new String[] {"10..15", "1..3"}
+         );
+         */
 
+        System.out.println("Results\n================");
+        for (int i = 0; i < files.size(); i++) {
+            System.out.println("files[" + i + "] = " + files.get(i));
+        }
+
+        //int[] test1 = breakAlphaSequence("A:a..f");
+    }
+}
